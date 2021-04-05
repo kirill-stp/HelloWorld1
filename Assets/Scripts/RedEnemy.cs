958e11743c6f9ee633dc710c15c8efb63d0655ce
Kirill Stepanishin <kirill.stpn.by@gmail.com>
2021-04-05 18:57:47 +0300

    Homework 1, 1.1, 1.2 (player-enemy)

---
+++ Assets/Scripts/RedEnemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class RedEnemy : MonoBehaviour
+{
+    [SerializeField] private RedPlayer player;
+    
+    [SerializeField]private string playerName = "Вова";
+    [SerializeField]private int attack = 6;
+    [SerializeField]private int hp = 40;
+    [SerializeField]private int pierce = 1;
+
+    private int tempHp;
+
+    [SerializeField]private Sprite sprite;
+    [SerializeField]private Image image;
+    [SerializeField]private TextMeshProUGUI nameText;
+    [SerializeField]private TextMeshProUGUI hpText;
+
+    public void TakeDamage(int damageValue)
+    {
+        if (damageValue < 0) damageValue = 0;
+        tempHp -= damageValue;
+        print($"Player deals {damageValue} damage to Enemy.\n{tempHp} hp left");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetValues();
+        image.sprite = sprite;
+        nameText.text = playerName;
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            player.TakeDamage(attack + pierce);
+            player.UpdateUI();
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) ResetValues();
+    }
+
+    public void UpdateUI()
+    {
+        hpText.text = tempHp.ToString();
+    }
+    private void ResetValues()
+    {
+        print("Restart");
+        tempHp = hp;
+        UpdateUI();
+        player.UpdateUI();
+    }
+}
---
+++ Assets/Scripts/RedPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RedPlayer : MonoBehaviour
+{
+    [SerializeField] private RedEnemy enemy;
+    
+    [SerializeField]private string enemyName = "Вася Пупкин";
+    [SerializeField]private int attack = 5;
+    [SerializeField]private int hp = 50;
+    [SerializeField]private int defence = 2;
+    
+    private int tempHp;
+
+    [SerializeField]private Sprite sprite;
+    [SerializeField]private Image image;
+    [SerializeField]private TextMeshProUGUI nameText;
+    [SerializeField]private TextMeshProUGUI hpText;
+
+    public void TakeDamage(int damageValue)
+    {
+        int damage = damageValue - defence;
+        if (damage < 0) damage = 0;
+        tempHp -= damage;
+        print($"Enemy deals {damage} damage to Player.\n{tempHp} hp left");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetValues();
+        image.sprite = sprite;
+        nameText.text = enemyName;
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            enemy.TakeDamage(attack);
+            enemy.UpdateUI();
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) ResetValues();
+    }
+    
+    public void UpdateUI()
+    {
+        hpText.text = tempHp.ToString();
+    }
+
+    private void ResetValues()
+    {
+        tempHp = hp;
+        UpdateUI();
+        enemy.UpdateUI();
+    }
+}
